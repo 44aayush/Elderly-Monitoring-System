8e6d975c0ee59e747fa0287cc10459cb249ebe66
44aayush <k44aayush@gmail.com>
2020-10-21 12:31:05 +0530

    App 1.0v

---
+++ App/src/main/java/com/login_signup_screendesign_demo/Login_Fragment.java
@@ -0,0 +1,199 @@
+package com.login_signup_screendesign_demo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.login_signup_screendesign_demo.R;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.content.res.XmlResourceParser;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class Login_Fragment extends Fragment implements OnClickListener {
+	private View view;
+	private  EditText emailid, password;
+	private  Button loginButton;
+	private  TextView forgotPassword, signUp;
+	private  CheckBox show_hide_password;
+	private  LinearLayout loginLayout;
+	private  Animation shakeAnimation;
+	private  FragmentManager fragmentManager;
+
+	public Login_Fragment() {
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		view = inflater.inflate(R.layout.login_layout, container, false);
+		initViews();
+		setListeners();
+		return view;
+	}
+
+	// Initiate Views
+	private void initViews() {
+		fragmentManager = getActivity().getSupportFragmentManager();
+
+		emailid = (EditText) view.findViewById(R.id.login_emailid);
+		password = (EditText) view.findViewById(R.id.login_password);
+		loginButton = (Button) view.findViewById(R.id.loginBtn);
+		forgotPassword = (TextView) view.findViewById(R.id.forgot_password);
+		signUp = (TextView) view.findViewById(R.id.createAccount);
+		show_hide_password = (CheckBox) view
+				.findViewById(R.id.show_hide_password);
+		loginLayout = (LinearLayout) view.findViewById(R.id.login_layout);
+
+		// Load ShakeAnimation
+		shakeAnimation = AnimationUtils.loadAnimation(getActivity(),
+				R.anim.shake);
+
+		// Setting text selector over textviews
+		@SuppressLint("ResourceType") XmlResourceParser xrp = getResources().getXml(R.drawable.text_selector);
+		try {
+			ColorStateList csl = ColorStateList.createFromXml(getResources(),
+					xrp);
+
+			forgotPassword.setTextColor(csl);
+			show_hide_password.setTextColor(csl);
+			signUp.setTextColor(csl);
+		} catch (Exception e) {
+		}
+	}
+
+	// Set Listeners
+	private void setListeners() {
+		loginButton.setOnClickListener(this);
+		forgotPassword.setOnClickListener(this);
+		signUp.setOnClickListener(this);
+
+		// Set check listener over checkbox for showing and hiding password
+		show_hide_password
+				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+					@Override
+					public void onCheckedChanged(CompoundButton button,
+							boolean isChecked) {
+
+						// If it is checkec then show password else hide
+						// password
+						if (isChecked) {
+
+							show_hide_password.setText(R.string.hide_pwd);// change
+																			// checkbox
+																			// text
+
+							password.setInputType(InputType.TYPE_CLASS_TEXT);
+							password.setTransformationMethod(HideReturnsTransformationMethod
+									.getInstance());// show password
+						} else {
+							show_hide_password.setText(R.string.show_pwd);// change
+																			// checkbox
+																			// text
+
+							password.setInputType(InputType.TYPE_CLASS_TEXT
+									| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+							password.setTransformationMethod(PasswordTransformationMethod
+									.getInstance());// hide password
+
+						}
+
+					}
+				});
+	}
+	public void startSpecificActivity(Class<?> otherActivityClass) {
+		Intent intent = new Intent(getActivity(), otherActivityClass);
+		startActivity(intent);
+	}
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.loginBtn:
+		int n=checkValidation();
+			if (n==1){
+				startSpecificActivity(Main2Activity.class);
+			}
+			break;
+
+		case R.id.forgot_password:
+
+			// Replace forgot password fragment with animation
+			fragmentManager
+					.beginTransaction()
+					.setCustomAnimations(R.anim.right_enter, R.anim.left_out)
+					.replace(R.id.frameContainer,
+							new ForgotPassword_Fragment(),
+							Utils.ForgotPassword_Fragment).commit();
+			break;
+
+		case R.id.createAccount:
+
+			// Replace signup frgament with animation
+			fragmentManager
+					.beginTransaction()
+					.setCustomAnimations(R.anim.right_enter, R.anim.left_out)
+					.replace(R.id.frameContainer, new SignUp_Fragment(),
+							Utils.SignUp_Fragment).commit();
+			break;
+
+
+		}
+
+	}
+
+	// Check Validation before login
+	private int checkValidation() {
+		// Get email id and password
+		String getEmailId = emailid.getText().toString();
+		String getPassword = password.getText().toString();
+
+		// Check patter for email id
+		Pattern p = Pattern.compile(Utils.regEx);
+
+		Matcher m = p.matcher(getEmailId);
+
+		// Check for both field is empty or not
+		if (getEmailId.equals("") || getEmailId.length() == 0
+				|| getPassword.equals("") || getPassword.length() == 0) {
+			loginLayout.startAnimation(shakeAnimation);
+			new CustomToast().Show_Toast(getActivity(), view,
+					"Enter both credentials.");
+			return 0;
+		}
+		// Check if email id is valid or not
+		else if (!m.find()){
+			new CustomToast().Show_Toast(getActivity(), view,
+					"Your Email Id is Invalid.");
+			return 0;
+		}
+		// Else do login and do your stuff
+		else{
+
+			return 1;
+		}
+	}
+}
